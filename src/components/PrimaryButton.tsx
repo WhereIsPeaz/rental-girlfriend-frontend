fdd20c8d8e9bfd495f880595b8a105d734f8a96b
ssirawiitch <sirawitchchairuangsirikul@gmail.com>
2025-10-26 23:49:58 +0700

    feat: add shared and card folder in components folder and fix Card.tsx to use components

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,7 +1,10 @@
-export default function PrimaryButton({ title }: { title: string }) {
+"use client"
+
+export default function PrimaryButton({ title, onClick }: { title: string; onClick: () => void }) {
     return (
         <button
-            type="submit"
+            type="button"
+            onClick={onClick}
             className="mr-1 cursor-pointer rounded-md bg-gradient-to-r from-pink-600 to-rose-500 px-4 py-2 text-sm font-semibold text-white transition-all duration-300 hover:scale-105"
         >
             {title}
